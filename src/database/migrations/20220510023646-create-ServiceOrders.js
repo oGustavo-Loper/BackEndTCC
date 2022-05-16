047221cf5113bf4a6e5810b63d34001d2d520c6f
LucasBGarcia <barbozagarcia@yahoo.com.br>
2022-05-16 19:51:53 -0300

    Add ServiceOrder controller, model and migration

---
+++ src/database/migrations/20220510023646-create-ServiceOrders.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('ServiceOrders', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      observation: {
+        type: Sequelize.STRING,
+      },
+      withdrawal: {
+        type: Sequelize.DATE,
+      },
+      value: {
+        type: Sequelize.FLOAT,
+      },
+      client_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'clients', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      service_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'services', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      brand_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'brands', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      modelo_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'modelos', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('ServiceOrders');
+  }
+};
